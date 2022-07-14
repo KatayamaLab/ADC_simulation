5aae733005064ba0ef0e72d93153feb94a90b5ff
ta-bo-0419 <7321540@ed.tus.ac.jp>
2022-07-14 17:06:20 +0900

    simulation

--- simapp.py
+++ simapp.py
@@ -6,31 +6,45 @@ import random
 class App:
     def __init__(self):
         st.title("ADC simulation")
+        self.side()
         t, wave, freq, amp = self.signal()
         self.graph(t, wave, freq, amp)
-        
-    
+            
     def signal(self):
-        N = 8192        # サンプル数
-        dt = 0.0001          # サンプリング周期 [s]
-        f1 = 50    # 周波数 [Hz]
 
-        t = np.arange(0, N*dt, dt) # 時間 [s]
-        x = 100 * np.sin(2*np.pi*f1*t)
-        wave = []
-        
-        for i in range(len(x)):
-            tmp = x[i] + random.uniform(-0.1,0.1)
-            wave.append(tmp)
-        
+        t = np.arange(0, self.N * self.dt, self.dt) # 時間 [s]
+        x = 100 * np.sin(2*np.pi*self.f*t)
+        wave = [i + random.uniform(-1*self.r, self.r) for i in x]  
         f = np.fft.fft(wave)
-        freq = np.linspace(0, 1/dt, N)
-        amp = np.abs(f) * (2 / N)       
+        freq = np.linspace(0, 1/self.dt, self.N)
+        amp = np.abs(f) * (2 / self.N)       
         
         return t, wave, freq, amp 
+    
+    def side(self):
+        st.sidebar.header("各種変数の設定")
+        
+        self.f = st.sidebar.slider("正弦波の周波数[Hz]", min_value=49.0, max_value=51.0, value=50.0)
+        st.sidebar.write(f"現在の周波数は{self.f} Hzです")
+        
+        self.i = st.sidebar.slider("サンプル数", min_value=5, max_value=15, value=10)
+        self.N = 2 ** self.i
+        st.sidebar.write(f"現在のサンプル数は{self.N}点です")
+        
+        self.j = st.sidebar.slider("サンプリング周期", min_value=1, max_value=6, value=3)
+        self.dt = 1 * 10 ** (-1 * self.j)
+        st.sidebar.write(f"現在のサンプリング周期は{self.dt} sです")
+        
+        self.l = st.sidebar.slider("表示範囲", min_value=1, max_value=10, value=1)
+        self.lim = 100 * self.l        
+        st.sidebar.write(f"現在の表示範囲は 0<f<{self.lim} です")
+        
+        self.r = st.sidebar.slider("ランダムなノイズ[V]", min_value=0.0, max_value=10.0, value=0.0)
         
     def graph(self, t, wave, freq, amp):
-        fig = plt.figure(figsize=(16,9))
+        fig = plt.figure(figsize=(20,12))
+        plt.subplots_adjust(wspace=0, hspace=0.3)
+        plt.rcParams["font.size"] = 18
         
         ax1 = fig.add_subplot(2,1,1)
         ax1.plot(t, wave, label="signal")
@@ -46,7 +60,7 @@ class App:
         ax2.set_ylabel("amplitude[V]")
         ax2.grid()
         ax2.legend()
-        ax2.set_xlim(0,100)
+        ax2.set_xlim(0,self.lim)
         
         st.pyplot(fig)
         
